684d8c87b4d380191977bfa8f53f72c9e3763ae2
Armin Herling <armin.herling@gmail.com>
2018-05-07 21:55:58 +0200

    Exercise 5.2

---
+++ IllustratedGuideToPython3/Chapter 5/Exercise 2.py
@@ -0,0 +1,7 @@
+# Exercise: Create a program that will ask a user how old they are.  Print out
+# some text telling them how old they will be next year.
+
+currentAge = input('Please enter your age:')
+nextYearAge = int(currentAge) + 1
+
+print('You will be', nextYearAge, 'years old, next year.');
