c9968b667b977a61878fe9efc729048ec25d96e3
Armin Herling <armin.herling@gmail.com>
2018-05-07 21:52:23 +0200

    Exercise 5.1

--- IllustratedGuideToPython3/Chapter 5/Exercise 1.py
+++ IllustratedGuideToPython3/Chapter 5/Exercise 1.py
@@ -1,1 +1,5 @@
+# Exercise: Create some Python code that will prompt you to enter your name. Print out Hello and then the name.
 
+userName = input('Please enter your name:')
+
+print('Hello',userName);
