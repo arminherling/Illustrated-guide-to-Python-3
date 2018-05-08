6790deb36eba5b05e98e9cf6e05804b472617146
Armin Herling <armin.herling@gmail.com>
2018-05-08 20:09:53 +0200

    Exercise 6.2

---
+++ IllustratedGuideToPython3/Chapter 6/Exercise 2.py
@@ -0,0 +1,13 @@
+# Exercise: Create a variable, b, that points to the base of a rectangle with a
+# value of 10.  Create a variable, h, that points to the height of a rectangle
+# with a value of 2.  Calculate the perimeter.  Change the base to 6 and
+# calculate the perimeter again.
+
+b = 10
+h = 2
+perimeter = (b + h) * 2  
+print("The perimeter of a rectangle with b = 10 and h = 2 is", perimeter)
+
+b = 6
+perimeter = (b + h) * 2  
+print("The perimeter of a rectangle with b = 6 and h = 2 is", perimeter)
