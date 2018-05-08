06ca8dd5cc4e8a66df134c9145fb30d105e9832b
Armin Herling <armin.herling@gmail.com>
2018-05-08 22:25:28 +0200

    Exercise 8.1

---
+++ IllustratedGuideToPython3/Chapter 8/Exercise 1.py
@@ -0,0 +1,6 @@
+# Exercise: You slept for 6.2, 7, 8, 5, 6.5, 7.1, and 8.5 hours this week.
+# Calculate the average number of hours slept.
+sum = 6.2 + 7 + 8 + 5 + 6.5 + 7.1 + 8.5
+average = sum / 7
+
+print('Your average number of hours of sleep this week was', average)
