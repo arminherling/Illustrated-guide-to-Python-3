37dd8e4b5d31003320181e5d2a2b75027141c782
Armin Herling <armin.herling@gmail.com>
2018-05-08 20:01:27 +0200

    Exercise 6.1

---
+++ IllustratedGuideToPython3/Chapter 6/Exercise 1.py
@@ -0,0 +1,9 @@
+# Exercise: Create a variable, pi, that points to an approximation for the
+# value of π.  Create a variable, r, for the radius of a circle that has a
+# value of 10.  Calculate the area of the circle (π times the radius squared).
+
+pi = 3.14
+r = 10
+
+area = pi * r ** 2
+print('The area of a circle with r = 10 is', area);
