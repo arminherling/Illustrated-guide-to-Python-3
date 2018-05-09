28d4080f632653e40dd834e7de0807ec48e3ba70
Armin Herling <armin.herling@gmail.com>
2018-05-09 22:26:40 +0200

    Exercise 9.2

---
+++ IllustratedGuideToPython3/Chapter 9/Exercise 2.py
@@ -0,0 +1,8 @@
+# Exercise: Create a variable, paragraph, that has the following content:
+# ”Python is a great language!”, said Fred. ”I don’t ever remember having this
+# much fun before.”
+
+paragraph = '''"Python is a great language!", said Fred. "I don´t ever remember having this
+much fun before."'''
+
+print(paragraph)
