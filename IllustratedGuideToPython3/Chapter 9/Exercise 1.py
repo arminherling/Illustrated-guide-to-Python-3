1f4e07f977e07fcb2e47efe680d4a9b5247dcf2d
Armin Herling <armin.herling@gmail.com>
2018-05-09 22:23:07 +0200

    Exercise 9.1

---
+++ IllustratedGuideToPython3/Chapter 9/Exercise 1.py
@@ -0,0 +1,11 @@
+# Exercise : Create a variable, name, pointing to your name.  Create another
+# variable, age, holding an integer value for your age.  Print out a string
+# formatted with both values.  If your name was Fred and age was 23 it would
+# print:
+# Fred is 23
+
+name = "Armin Herling"
+age = 29
+sentence = f"{name} is {age}."
+
+print(sentence)
