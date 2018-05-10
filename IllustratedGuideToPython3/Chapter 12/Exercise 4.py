e266fc3f3bb967479d57ad0801a5a9bad6b818e3
Armin Herling <armin.herling@gmail.com>
2018-05-10 18:36:12 +0200

    Exercise 12.4

---
+++ IllustratedGuideToPython3/Chapter 12/Exercise 4.py
@@ -0,0 +1,8 @@
+# Exercise: Create a variable, car, set to None. Write code to print 'Taxi for you!', or 'You
+# have a car!', based on whether or not car is set (None is not the name of a car).
+
+car = None
+if (car):
+    print("You have a car!")
+else:
+    print("Taxi for you!")
