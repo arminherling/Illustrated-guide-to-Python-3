c253cd1c4af9005df61eebc0ca07fd358faded38
Armin Herling <armin.herling@gmail.com>
2018-05-10 15:45:47 +0200

    Exercise 11.1

---
+++ IllustratedGuideToPython3/Chapter 11/Exercise 1.py
@@ -0,0 +1,8 @@
+# Exercise : Create a string, school, with the name of your elementary school. Examine the
+# methods that are available on that string. Use the help function to view their
+# documentation.
+
+school = "Elementary school name"
+print(dir(school))
+
+print(help(school.count))
