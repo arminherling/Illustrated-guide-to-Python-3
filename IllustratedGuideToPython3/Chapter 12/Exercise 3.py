d2957fce1c4da8f73077549e2b6c37fcaefd346f
Armin Herling <armin.herling@gmail.com>
2018-05-10 18:33:53 +0200

    Exercise 12.3

---
+++ IllustratedGuideToPython3/Chapter 12/Exercise 3.py
@@ -0,0 +1,9 @@
+# Exercise: Create a list, names, with the names of people in a class. Write code to print 'The
+# class is empty!' or 'Class has enrollments.', based on whether there are
+# values in names. (See the tip in this chapter for details).
+
+names = []
+if (names):
+    print("Class has entollments.")
+else:
+    print("The class is empty!")
