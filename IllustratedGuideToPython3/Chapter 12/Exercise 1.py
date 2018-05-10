16d8bc39dc245080af2ac6b9014ae19cfa3cf241
Armin Herling <armin.herling@gmail.com>
2018-05-10 17:52:56 +0200

    Exercise 12.1

---
+++ IllustratedGuideToPython3/Chapter 12/Exercise 1.py
@@ -0,0 +1,7 @@
+# Exercise: Create a variable, age, set to your age. Create another variable, old, that uses a
+# condition to test whether you are older than 18. The value of old should be True
+# or False.
+
+age = 29
+old = age > 18
+print(old)
