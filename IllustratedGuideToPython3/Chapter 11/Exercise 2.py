954a5a18eb9f1aa8f212db0337ca789bff0a7f57
Armin Herling <armin.herling@gmail.com>
2018-05-10 15:52:05 +0200

    Exercise 11.2

---
+++ IllustratedGuideToPython3/Chapter 11/Exercise 2.py
@@ -0,0 +1,7 @@
+# Exercise: Create a string, country, with the value 'usa'. Create a new string, correct_
+# country, that has the value in uppercase, by using a string method.
+
+country = "usa"
+correct_country = country.upper()
+
+print(correct_country)
