f356fb4d10802cf4d7255f6b0ca2922e0008ecd3
Armin Herling <armin.herling@gmail.com>
2018-05-16 20:20:36 +0200

    Exercise 16.1

---
+++ IllustratedGuideToPython3/Chapter 16/Exercise 1.py
@@ -0,0 +1,8 @@
+#Exercise: Create a dictionary, info, that holds your first name, last name, and age.
+
+person = {"first": "Armin", "second": "Herling", "age": 29}
+
+print(person["first"])
+print(person["second"])
+print(person["age"])
+print(person)
