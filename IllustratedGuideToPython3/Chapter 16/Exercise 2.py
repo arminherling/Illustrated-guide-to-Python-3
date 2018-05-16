8fdf642e1d46f0ee4e5220554adcaa2ba347ae59
Armin Herling <armin.herling@gmail.com>
2018-05-16 20:34:57 +0200

    Exercise 16.2

---
+++ IllustratedGuideToPython3/Chapter 16/Exercise 2.py
@@ -0,0 +1,10 @@
+# Exercise: Create an empty dictionary, phone, that will hold details about your phone. Add the
+# screen size, memory, OS, brand, etc. to the dictionary.
+
+phone = {}
+phone["screen_size"] = 7
+phone["memory"] = "2 GB"
+phone["OS"] = "Android"
+phone["brand"] = "ZTE"
+
+print(phone)
