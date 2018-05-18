c684a6b6cbb077e5dada6a85c9a5d1c64e607aa2
Armin Herling <armin.herling@gmail.com>
2018-05-18 23:55:35 +0200

    Exercise 18.1

---
+++ IllustratedGuideToPython3/Chapter 18/Exercise 1.py
@@ -0,0 +1,9 @@
+# Exercise: Create a variable with your name stored as a string. Use indexing operations to pull
+# out the first character. Pull out the last character. Will your code to pull out the last
+# character work on a name of arbitrary length?
+
+name = "Armin Herling"
+
+print("Full name:", name)
+print("First character:", name[0])
+print("Last character:", name[-1])
